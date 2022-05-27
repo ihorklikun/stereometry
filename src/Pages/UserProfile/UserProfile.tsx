874aca9780a8101ec51fbdbd37e4e81ce02eecc4
Ihor Klikun <klikun2001@gmail.com>
2022-05-28 01:30:20 +0300

    Init UserProfile Page

---
+++ src/Pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,8 @@
+import "./UserProfile.css";
+import React, { useRef, useState, FC, useEffect } from "react";
+
+interface UserProfileProps {}
+const UserProfile: FC<UserProfileProps> = (props) => {
+  return <></>;
+};
+export default UserProfile;
